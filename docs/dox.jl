af5b51b21c04de03664a4d42a6cf8569c08b56ac
Wen Wei Tseng <sosiristseng@gmail.com>
2025-02-07 23:59:24 +0800

    DOX addition

--- docs/dox.jl
+++ docs/dox.jl
@@ -49,56 +49,38 @@ plot(sol1, idxs=idxs, lab="DOX=260, ρC4=500", title="PM potential")
 #---
 plot(sol2, idxs=idxs, lab="DOX=260, ρC3=500", title="PM potential")
 
-@unpack atp_i, adp_i, vm, na_o, na_i, atp_i, adp_i, atp_m, adp_m, sox_i, sox_m = sys
-@unpack ca_i, ca_nsr, ca_jsr, ca_ss = sys
-plot(sol0, idxs=vm, label="C4 1x", title="PM potential")
-plot(sol0, idxs=[atp_i / adp_i], label="C4 1x", title="ATP:ADP", density=1000)
+#---
+idxs = (sys.t/1000, [sys.atp_i / sys.adp_i])
+tspan=(100second, 200second)
+plot(sol0, idxs=idxs, label="C4 325", title="ATP:ADP"; tspan)
+plot!(sol1, idxs=idxs, label="C4 500", line=:dash; tspan)
+plot!(sol2, idxs=idxs, label="C3 500", line=:dot; tspan)
 
-plot(sol1, idxs=vm, label="C4 3x")
-plot(sol2, idxs=vm, label="C3 3x")
-
-p1 = plot(sol, idxs=[ca_i, ca_ss], tspan=(990, tend))
-p2 = plot(sol, idxs=[ca_nsr, ca_jsr], tspan=(990, tend))
-plot(p1, p2, layout=(2, 1))
-@unpack IK1, IK, IKp, IKatp, INa, INaCa, ICaL = sys
-plot(sol0, idxs=[IK1, IK, IKp, IKatp, ICaL], tspan=(400second, tend))
-
-plot(sol0, idxs=[atp_i / adp_i], label="C4 1x", title="ATP:ADP")
-plot!(sol1, idxs=[atp_i / adp_i], label="C4 3x", line=:dash)
-plot!(sol2, idxs=[atp_i / adp_i], label="C3 3x", line=:dot)
-
-@unpack vHres, vHu, vANT, vHleak, vNaCa, vUni, vIMAC, vROS, vTrROS, vO2 = sys
+#---
 @unpack Q_n, Qdot_n, QH2_n, QH2_p, Qdot_p, Q_p, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
-plot(sol0, idxs=[Q_n, Qdot_n, QH2_n, QH2_p, Qdot_p, Q_p], title="C4 1x")
-plot(sol1, idxs=[Q_n, Qdot_n, QH2_n, QH2_p, Qdot_p, Q_p], title="C4 3x")
-plot(sol2, idxs=[Q_n, Qdot_n, QH2_n, QH2_p, Qdot_p, Q_p], title="C3 3x")
+plot(sol0, idxs=[Q_n, Qdot_n, QH2_n, QH2_p, Qdot_p, Q_p], title="Q cycle", legend=:right)
 
-plot(sol0, idxs=[sys.dpsi * 1000], title="C4 1x", tspan=(700, 800))
-plot!(sol1, idxs=[sys.dpsi * 1000], title="C4 3x", tspan=(700, 800))
-plot!(sol2, idxs=[sys.dpsi * 1000], title="C3 3x", ylims=(100, 160), tspan=(700, 800))
+#---
+idxs = (sys.t/1000, sys.dpsi)
+tspan=(100second, 200second)
+plot(sol0, idxs=idxs, title="MMP", lab="C4 325" ; tspan)
+plot!(sol1, idxs=idxs, label="C4 500"; tspan)
+plot!(sol2, idxs=idxs, label="C3 500"; tspan, xlabel="Time (s)", ylabel="MMP (mV)")
 
-plot(sol, idxs=[vHres, vHu, vANT, vHleak, vNaCa, vUni, vTrROS], ylims=(0, 5))
-plot(sol, idxs=vANT, tspan=(400, 500))
-plot(sol, idxs=[fes_ox / fes_rd], tspan=(400, 500))
-plot(sol, idxs=[cytc_ox / cytc_rd], tspan=(400, 500))
-plot(sol, idxs=[sys.sox_i, sys.h2o2_i], tspan=(740, 800))
-plot(sol, idxs=[sys.vSOD_i, sys.vGPX_i, sys.vGR_i, sys.vCAT], tspan=(400, 500))
-plot(sol0, idxs=[sys.vROSC1, sys.vROSC3], tspan=(700, 800), title="C4 1x")
-plot(sol1, idxs=[sys.vROSC1, sys.vROSC3], tspan=(700, 800), title="C4 3x")
-plot(sol2, idxs=[sys.vROSC1, sys.vROSC3], tspan=(700, 800), title="C3 3x")
+#---
+idxs = (sys.t/1000, sys.vO2)
+plot(sol0, idxs=idxs, label="C4 325", title="Oxygen consumption")
+plot!(sol1, idxs=idxs, label="C4 500", line=:dash)
+plot!(sol2, idxs=idxs, label="C3 500", line=:dot, xlabel="Time (s)", ylabel="vO2 (μM/ms)")
 
-plot(sol0, idxs=vO2, label="C4 1x", title="vO2")
-plot!(sol1, idxs=vO2, label="C4 3x", line=:dash)
-plot!(sol2, idxs=vO2, label="C3 3x", line=:dot)
+#---
+idxs = (sys.t/1000, sys.vROS / (sys.vO2 + sys.vROS))
+plot(sol0, idxs=idxs, label="C4 325", title="ROS vO2 fraction")
+plot!(sol1, idxs=idxs, label="C4 500", line=:dash)
+plot!(sol2, idxs=idxs, label="C3 500", line=:dot, xlabel="Time (s)")
 
-plot(sol0, idxs=vROS, label="C4 1x", title="vROS")
-plot!(sol1, idxs=vROS, label="C4 3x")
-
-plot(sol, idxs=[sys.vROS / (sys.vO2 + sys.vROS)])
-
-@unpack nad_m, nadh_m, vC1, vIDH, vKGDH, vMDH = sys
-plot(sol, idxs=[nad_m / nadh_m])
-
-@unpack cit, isoc, oaa, akg, scoa, suc, fum, mal = sys
-@unpack vCS, vACO, vIDH, vKGDH, vSL, vFH, vMDH, vAAT, vSDH = sys
-plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal])
+#---
+idxs = (sys.t/1000, sys.nadh_m)
+plot(sol0, idxs=idxs, label="C4 325", title="NADH")
+plot!(sol1, idxs=idxs, label="C4 500", line=:dash)
+plot!(sol2, idxs=idxs, label="C3 500", line=:dot, xlabel="Time (s)")
