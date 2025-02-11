0a070bb740404c898eb77efa8dac027921c06347
Wen-Wei Tseng <sosiristseng@gmail.com>
2025-02-11 13:24:10 +0000

    cicr

--- src/cicr16.jl
+++ src/cicr16.jl
@@ -1,13 +1,13 @@
-# Good old CICR, including L-type calcium channel (LCC) and Ryanodine receptor (RyR)
+# Good old 16-state CICR (Cortassa 2006), including L-type calcium channel (LCC) and Ryanodine receptor (RyR)
 "LCC ODE system"
-function get_lcc_sys(ca_ss, ca_o, k_i, k_o, vm; name=:lccsys)
+function get_cicr06_sys(ca_ss, ca_o, k_i, k_o, vm; name=:cicrsys)
     @parameters begin
         A_LCC = 2
         B_LCC = 2
-        ω_LCC = 0.01/ms
-        f_LCC = 0.3/ms
-        g_LCC = 2/ms
-        γ_LCC = 0.1875/(ms * mM)
+        ω_LCC = 0.01 / ms
+        f_LCC = 0.3 / ms
+        g_LCC = 2 / ms
+        γ_LCC = 0.1875 / (ms * mM)
         P_CA_LCC = 1.24E-3cm * Hz
         P_K_LCC = 1.11E-11cm * Hz
         I_CA_HALF_LCC = -0.4583μAcm⁻²
@@ -25,7 +25,7 @@ function get_lcc_sys(ca_ss, ca_o, k_i, k_o, vm; name=:lccsys)
         c4_lcc(t) = 1.749e-20
         # Opened LCC state
         o_lcc(t) = 2.624e-20
-        # Ca-inhibited LCC state
+        # Ca-inhibited LCC states
         cca0_lcc(t) = 1.1328e-3
         cca1_lcc(t) = 1.7591e-8
         cca2_lcc(t) = 6.3826e-13
@@ -41,43 +41,42 @@ function get_lcc_sys(ca_ss, ca_o, k_i, k_o, vm; name=:lccsys)
     end
 
     v = vm / mV
-    A, B = A_LCC, B_LCC
     ω, f, g = ω_LCC, f_LCC, g_LCC
     α = α_lcc
     β = β_lcc
-    α′ = α * A
-    β′ = β / B
+    α′ = 2α
+    β′ = β / 2
     γ = γ_LCC * ca_ss
-    v01 = 4 * α * c0_lcc - β * c1_lcc
-    v12 = 3 * α * c1_lcc - 2 * β * c2_lcc
-    v23 = 2 * α * c2_lcc - 3 * β * c3_lcc
-    v34 = α * c3_lcc - 4 * β * c4_lcc
-    v45 = f * c4_lcc - g * o_lcc
-    v67 = 4 * α′ * cca0_lcc - β′ * cca1_lcc
-    v78 = 3 * α′ * cca1_lcc - 2 * β′ * cca2_lcc
-    v89 = 2 * α′ * cca2_lcc - 3 * β′ * cca3_lcc
-    v910 = α′ * cca3_lcc - 4 * β′ * cca4_lcc
-    v06 = γ * c0_lcc - ω * cca0_lcc
-    v17 = A * γ * c1_lcc - ω / B * cca1_lcc
-    v28 = (A^2) * γ * c2_lcc - ω / (B^2) * cca2_lcc
-    v39 = (A^3) * γ * c3_lcc - ω / (B^3) * cca3_lcc
-    v410 = (A^4) * γ * c4_lcc - ω / (B^4) * cca4_lcc
+    vc0c1 = 4α * c0_lcc - β * c1_lcc
+    vc1c2 = 3α * c1_lcc - 2β * c2_lcc
+    vc2c3 = 2α * c2_lcc - 3β * c3_lcc
+    vc3c4 = α * c3_lcc - 4β * c4_lcc
+    vc4o = f * c4_lcc - g * o_lcc
+    vca0ca1 = 4α′ * cca0_lcc - β′ * cca1_lcc
+    vca1ca2 = 3α′ * cca1_lcc - 2β′ * cca2_lcc
+    vca2ca3 = 2α′ * cca2_lcc - 3β′ * cca3_lcc
+    vca3ca4 = α′ * cca3_lcc - 4β′ * cca4_lcc
+    vc0ca0 = γ * c0_lcc - ω * cca0_lcc
+    vc1ca1 = 2γ * c1_lcc - ω / 2 * cca1_lcc
+    vc2ca2 = 4γ * c2_lcc - ω / 4 * cca2_lcc
+    vc3ca3 = 8γ * c3_lcc - ω / 8 * cca3_lcc
+    vc4ca4 = 16γ * c4_lcc - ω / 16 * cca4_lcc
 
     eqs = [
-        α_lcc ~ 0.4/ms * exp((v + 2) / 10),
-        β_lcc ~ 0.05/ms * exp(-(v + 2) / 13),
+        α_lcc ~ 0.4 / ms * exp((v + 2) / 10),
+        β_lcc ~ 0.05 / ms * exp(-(v + 2) / 13),
         1 ~ c0_lcc + c1_lcc + c2_lcc + c3_lcc + c4_lcc + o_lcc + cca0_lcc + cca1_lcc + cca2_lcc + cca3_lcc + cca4_lcc,
-        # D(c0_lcc) ~ -v01 - v06,
-        D(c1_lcc) ~ v01 - v12 - v17,
-        D(c2_lcc) ~ v12 - v23 - v28,
-        D(c3_lcc) ~ v23 - v34 - v39,
-        D(c4_lcc) ~ v34 - v45 - v410,
-        D(o_lcc) ~ v45,
-        D(cca0_lcc) ~ v06 - v67,
-        D(cca1_lcc) ~ v17 + v67 - v78,
-        D(cca2_lcc) ~ v28 + v78 - v89,
-        D(cca3_lcc) ~ v39 + v89 - v910,
-        D(cca4_lcc) ~ v910 + v410,
+        # D(c0_lcc) ~ -vc0c1 - vc0ca0,
+        D(c1_lcc) ~ vc0c1 - vc1c2 - vc1ca1,
+        D(c2_lcc) ~ vc1c2 - vc2c3 - vc2ca2,
+        D(c3_lcc) ~ vc2c3 - vc3c4 - vc3ca3,
+        D(c4_lcc) ~ vc3c4 - vc4o - vc4ca4,
+        D(o_lcc) ~ vc4o,
+        D(cca0_lcc) ~ vc0ca0 - vca0ca1,
+        D(cca1_lcc) ~ vc1ca1 + vca0ca1 - vca1ca2,
+        D(cca2_lcc) ~ vc2ca2 + vca1ca2 - vca2ca3,
+        D(cca3_lcc) ~ vc3ca3 + vca2ca3 - vca3ca4,
+        D(cca4_lcc) ~ vca3ca4 + vc4ca4,
         y_inf ~ expit(-(v + 55) / 7.5) + 0.5 * expit((v - 21) / 6.0),
         τ_yca ~ 20ms + 600ms * expit(-(v + 30) / 9.5),
         D(x_yca) ~ (y_inf - x_yca) / τ_yca,
@@ -108,7 +107,7 @@ function get_ryr_sys(ca_jsr, ca_ss; name=:ryrsys)
         Jrel(t)
     end
 
-    vc1o1 =  KA_P_RYR * ca_ss^4 * pc1_ryr - KA_M_RYR * po1_ryr
+    vc1o1 = KA_P_RYR * ca_ss^4 * pc1_ryr - KA_M_RYR * po1_ryr
     vo1o2 = KB_P_RYR * ca_ss^3 * po1_ryr - KB_M_RYR * po2_ryr
     vo1c2 = KC_P_RYR * po1_ryr - KC_M_RYR * pc2_ryr
 
