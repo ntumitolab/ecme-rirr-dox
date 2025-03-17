424887909253559df920b456fdc80c7726f009b6
Wen Wei Tseng <sosiristseng@gmail.com>
2025-03-17 14:46:06 +0800

    move around

--- src/ECMEDox.jl
+++ src/ECMEDox.jl
@@ -23,18 +23,22 @@ include("oxphos.jl")
 include("transmito.jl")
 include("u0.jl")
 
-function build_model(; name, use_mg=false, simplify=true, bcl=1second, istim=-80μAcm⁻², tstart=0second, tend=10second, duty=0.5ms)
+function build_model(; name, use_mg=false, simplify=true, bcl=1second, istim=-80μAcm⁻², tend=1000second, tstart=zero(tend), duty=0.5ms)
     @parameters begin
-        iStim(t) = 0μAcm⁻²    # Stimulation current
-        DOX(t) = 0mM               # Doxorubicin concentration
+        iStim(t) = 0μAcm⁻²      # Stimulation current
+        DOX(t) = 0mM            # Doxorubicin concentration
+        O2(t) = 6μM             # Oxygen concentration
         MT_PROT = 1             # OXPHOS protein content
-        ΣA_m = 1.01mM           # Mitochondrial ATP + ADP pool (Gauthier-2013)
-        ΣA_i = 8mM              # Cytosolic ATP + ADP pool (Li-2015)
-        ΣNAD_m = 10mM           # Mitochondrial NAD + NADH pool # 1.0mM (Gauthier-2013)
+        ΣA_m = 1.01mM           # Mitochondrial ATP + ADP pool (Gauthier, 2013)
+        ΣA_i = 8mM              # Cytosolic ATP + ADP pool (Li, 2015)
+        ΣNAD_m = 10mM           # Mitochondrial NAD + NADH pool (Zhou, 2009) # Or 1.0mM (Gauthier, 2013)
         ΣNADP_m = 0.1mM         # Mitochondrial NADP + NADPH pool (Gauthier-2013)
 
+        # Membrane capacitance
         CM = 1μFcm⁻²          # Plasma membrane capacitance
+        iCM = inv(CM)
         CM_MITO = 1.812μM / mV  # Inner mitochondrial membrane capacitance
+        iCM_MITO = inv(CM_MITO)
 
         # Cell geometries
         A_CAP = 1.534E-4cm²
@@ -54,8 +58,8 @@ function build_model(; name, use_mg=false, simplify=true, bcl=1second, istim=-80
         KM_CA_CSQN = 0.8mM
         ET_CSQN = 5.0mM
         δCA = 3E-4          # Mito. Ca buffering factor
+
         # constant concentrations
-        O2(t) = 6μM          # Oxygen concentration
         h_i(t) = exp10(-7) * Molar   # Cytosolic proton concentration
         h_m(t) = exp10(-7.6) * Molar # Mitochondrial proton concentration
         nadph_i = 0.075mM   # Cytosolic NADPH (Gauthier-2013) # 1.0mM (Li-2015)
@@ -118,18 +122,18 @@ function build_model(; name, use_mg=false, simplify=true, bcl=1second, istim=-80
     @unpack vUni, vNaCa = mitocasys
 
     eqs = [
-        D(vm) ~ -inv(CM) * (INa + ICaL + IK + IK1 + IKp + INaCa + INaK + INsNa + IPMCA + ICaB + INaB + iStim + IKatp + ICaK),
-        D(na_i) ~ -A_CAP_V_MYO_F * (INa + INaB + INsNa + 3 * (INaCa + INaK)),
-        D(k_i) ~ -A_CAP_V_MYO_F * (IK + IK1 + IKp + IKatp + iStim - 2 * INaK + ICaK),
-        D(ca_i) ~ β_ca(ca_i, KM_CA_CMDN, ET_CMDN) * (Jxfer - Jup - Jtrpn - 0.5 * A_CAP_V_MYO_F * (IPMCA + ICaB - 2 * INaCa) + V_MITO_V_MYO * (vNaCa - vUni)),
+        D(vm) ~ -iCM * (INa + ICaL + IK + IK1 + IKp + INaCa + INaK + INsNa + IPMCA + ICaB + INaB + iStim + IKatp + ICaK),
+        D(na_i) ~ -A_CAP_V_MYO_F * (INa + INaB + INsNa + 3INaCa + 3INaK),
+        D(k_i) ~ -A_CAP_V_MYO_F * (IK + IK1 + IKp + IKatp + iStim - 2INaK + ICaK),
+        D(ca_i) ~ β_ca(ca_i, KM_CA_CMDN, ET_CMDN) * (Jxfer - Jup - Jtrpn - 0.5A_CAP_V_MYO_F * (IPMCA + ICaB - 2 * INaCa) + V_MITO_V_MYO * (vNaCa - vUni)),
         D(ca_nsr) ~ (V_MYO * Jup - V_JSR * Jtr) / V_NSR,
         D(ca_jsr) ~ β_ca(ca_jsr, KM_CA_CSQN, ET_CSQN) * (Jtr - Jrel),
-        D(ca_ss) ~ β_ca(ca_ss, KM_CA_CMDN, ET_CMDN) * ((V_JSR * Jrel - V_MYO * Jxfer) / V_SS - 0.5 * ICaL * A_CAP_V_SS_F),
+        D(ca_ss) ~ β_ca(ca_ss, KM_CA_CMDN, ET_CMDN) * ((V_JSR * Jrel - V_MYO * Jxfer) / V_SS - ICaL * 0.5A_CAP_V_SS_F),
         D(ca_m) ~ δCA * (vUni - vNaCa),
-        D(adp_i) ~ vCK_mito - V_MITO_V_MYO * vANT + vAm + 0.5 * Jup + A_CAP_V_MYO_F * (IPMCA + INaK),
+        D(adp_i) ~ vCK_mito - V_MITO_V_MYO * vANT + vAm + 0.5Jup + A_CAP_V_MYO_F * (IPMCA + INaK),
         D(adp_m) ~ vANT - vSL - vC5,
         D(nadh_m) ~ -vNADHC1 + vIDH + vKGDH + vMDH,
-        D(dpsi) ~ inv(CM_MITO) * (vHres - vHu - vANT - vHleak - vNaCa - 2 * vUni - vTrROS),
+        D(dpsi) ~ iCM_MITO * (vHres - vHu - vANT - vHleak - vNaCa - 2vUni - vIMAC),
         D(sox_m) ~ vROS - vTrROS,
         ΣA_i ~ atp_i + adp_i,
         ΣA_m ~ atp_m + adp_m,
