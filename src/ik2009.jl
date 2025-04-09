1a48b72338d85c6ff1b1e4e1674c0125e502012a
Wen Wei Tseng <sosiristseng@gmail.com>
2025-04-09 23:55:02 +0800

    name change (#32)

--- src/ik2009.jl
+++ src/ik2009.jl
@@ -10,7 +10,6 @@ function get_ik_sys(; na_i, na_o, k_i, k_o, mg_i, vm, atp_i, adp_i, name=:iksys)
     end
     @variables begin
         EK(t)               # Reversal potential of K
-        ΔVK(t)
         IK1(t)              # Time-independent K current
         IK(t)               # Time-dependent delayed rectifier K current
         IKp(t)              # Plateau K current
@@ -19,26 +18,27 @@ function get_ik_sys(; na_i, na_o, k_i, k_o, mg_i, vm, atp_i, adp_i, name=:iksys)
     end
 
     # Time-independent potassium current
+    ΔVK = vm - EK
     vk = ΔVK / mV
-    α1 = expit(-0.2385 * (vk - 59.215), 1.02)
-    β1 = (0.4912 * exp(0.08032 * (vk + 5.476)) + exp(0.06175 * (vk - 594.31))) * expit(0.5143 * (vk + 4.753))
+    α1 = 1.02 / (1 + exp(0.2385 * (vk - 59.215)))
+    β1 = (0.4912 * exp(0.08032 * (vk + 5.476)) + exp(0.06175 * (vk - 594.31))) / (1 + exp(-0.5143 * (vk + 4.753)))
     # Time-dependent delayed rectifier K current system
     α = 7.19e-5 / ms / (0.148) * exprel(-0.148 * (vm + 30))
     β = 1.31e-4 / ms / (0.0687) * exprel(0.0687* (vm + 30))
     E_KNa = nernst(na_o * P_NA_K + k_o, na_i * P_NA_K + k_i)
     x1 = expit(-inv(40) * (vm - 40))
     # ATP-dependent K channel (KATP) current
-    hatp = 1.3 + 0.74 * exp(-0.09 / μM * adp_i)   # Hill factor (Ferrero)
-    km_atp = 35.8μM + 17.9μM * NaNMath.pow(adp_i / μM, 0.256) # fixed, it's μM rather than mM
+    adp = adp_i / μM
+    hatp = 1.3 + 0.74 * exp(-0.09adp)   # Hill factor (Ferrero)
+    km_atp = 35.8μM + 17.9μM * NaNMath.pow(adp, 0.256) # fixed, it's μM rather than mM
     f_atp = hil(km_atp, atp_i, hatp)  # Inhibition by ATP
 
     eqs = [
         D(x_k) ~ α - x_k * (α + β),
         EK ~ nernst(k_o, k_i),
-        ΔVK ~ vm - EK,
         IK1 ~ G_K1 * (α1 / (α1 + β1)) * ΔVK,
         IK ~ G_K * x1 * x_k^2 * (vm - E_KNa),
-        IKp ~ G_KP * ΔVK * expit((vm - 7.488) / 5.98),
+        IKp ~ G_KP * ΔVK / (1 + exp(-(vm - 7.488) / 5.98)),
         IKatp ~ G0_KATP * f_atp * ΔVK
     ]
     return ODESystem(eqs, t; name)
