720b26144aebaac64ad104733b46d1325ded473b
Wen Wei Tseng <sosiristseng@gmail.com>
2025-04-12 17:26:11 +0800

    wip

--- c1-test.jl
+++ c1-test.jl
@@ -1,12 +1,13 @@
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as D
+using NaNMath
 using ECMEDox
 using ECMEDox: mM, μM, iVT, mV, Molar, Hz, ms
 using Plots
 
 # Adapted from Markevich, 2015
-function get_c1_sys(; name=:c1sys,
-    Qn=3.0mM, QH2_n=0.3mM,
+function c1_markevich(; name=:c1sys,
+    Q_n=3.0mM, QH2_n=0.3mM,
     nad=500μM, nadh=500μM,
     dpsi=150mV, O2=6μM, sox_m=0.001μM,
     h_i=exp10(-7) * Molar, h_m=exp10(-7.6) * Molar,
@@ -54,10 +55,12 @@ function get_c1_sys(; name=:c1sys,
         vROSIf(t)
         vROSIq(t)
         vROS_C1(t)
+        TN_C1(t) # Turnover number
+        ROSshunt_C1(t)
     end
 
     # Q association
-    v1 = K1_C1 * (I_C1 * Qn - Q_C1 / KEQ1_C1)
+    v1 = K1_C1 * (I_C1 * Q_n - Q_C1 / KEQ1_C1)
     # First electron transfer
     v2 = K2_C1 * (Q_C1 * N2m_C1 - SQ_C1 * N2_C1 / KEQ2_C1)
     # Second electron transfer
@@ -85,6 +88,8 @@ function get_c1_sys(; name=:c1sys,
         vROSIq ~ v6,
         vROS_C1 ~ vROSIf + vROSIq,
         vNADH_C1 ~ -0.5 * (v2 + v3 + v5),
+        TN_C1 ~ -vNADH_C1 / ET_C1,
+        ROSshunt_C1 ~ -vROS_C1 / vNADH_C1,
         ET_C1 ~ I_C1 + Q_C1 + SQ_C1 + QH2_C1,
         D(Q_C1) ~ v1 - v2 + v6,
         D(SQ_C1) ~ v2 - v3 - v6,
@@ -93,27 +98,123 @@ function get_c1_sys(; name=:c1sys,
     return ODESystem(eqs, t; name)
 end
 
+function c1_gauthier(; name=:c1sys,
+    Q_n=3.0mM, QH2_n=0.3mM, nad=500μM, nadh=500μM,
+    dpsi=150mV, O2=6μM, sox_m=0.001μM,
+    h_i=exp10(-7) * Molar, h_m=exp10(-7.6) * Molar,
+    C1_INHIB = 1)
+    @parameters begin
+        ET_C1 = 8.85mM      # Activity of complex I
+        dpsi_B_C1 = 50mV   # Phase boundary potential
+        # Transition rates
+        K12_C1 = 6339.6Hz # pH = 7
+        K21_C1 = 5Hz
+        K56_C1 = 100Hz
+        K65_C1 = 251190Hz # pH = 7
+        K61_C1 = 1e7Hz
+        K16_C1 = 130Hz
+        K23_C1 = 3886.7Hz / sqrt(mM)
+        K32_C1 = 9.1295e6Hz
+        K34_C1 = 639.1364Hz
+        K43_C1 = 3.2882Hz / sqrt(mM)
+        K47_C1 = 1.5962E7Hz/mM
+        K74_C1 = 65.2227Hz
+        K75_C1 = 24.615E3Hz
+        K57_C1 = 1.1667E3Hz / sqrt(mM)
+        K42_C1 = 6.0318Hz / mM
+        Em_O2_SOX = -160mV         # O2/Superoxide redox potential
+        Em_FMNH2_FMNH = -375mV     # FMNH/FMNH2 redox potential
+    end
+
+    @variables begin
+        C1_1(t) = 0
+        C1_2(t) # Conserved
+        C1_3(t) = 0
+        C1_4(t) = 0
+        C1_5(t) = 0
+        C1_6(t) = 0
+        C1_7(t) = 0
+        vQC1(t)
+        vNADHC1(t)
+        vROSC1(t)
+        TN_C1(t) # Turnover number
+        ROSshunt_C1(t)
+    end
+
+    fhi = h_i / 1E-7Molar
+    fhm = h_m / 1E-7Molar
+    fv = exp(iVT * (dpsi - dpsi_B_C1))
+    # State transition rates
+    a12 = K12_C1 * fhm^2
+    a21 = K21_C1
+    a65 = K65_C1 * fhi^2
+    a56 = K56_C1
+    a61 = K61_C1 / fv
+    a16 = K16_C1 * fv
+    a23 = K23_C1 * NaNMath.sqrt(nadh)
+    a32 = K32_C1
+    a34 = K34_C1
+    a43 = K43_C1 * NaNMath.sqrt(nad)
+    a47 = C1_INHIB * K47_C1 * NaNMath.sqrt(Q_n * h_m)
+    a74 = K74_C1
+    a57 = C1_INHIB * K57_C1 * NaNMath.sqrt(QH2_n)
+    a75 = K75_C1
+    a42 = K42_C1 * O2
+    a24 = K42_C1 * exp(iVT * (Em_FMNH2_FMNH - Em_O2_SOX)) * sox_m
+
+    v12 = a12 * C1_1 - a21 * C1_2
+    v23 = a23 * C1_2 - a32 * C1_3
+    v34 = a34 * C1_3 - a43 * C1_4
+    v47 = a47 * C1_4 - a74 * C1_7
+    v75 = a75 * C1_7 - a57 * C1_5
+    v56 = a56 * C1_5 - a65 * C1_6
+    v61 = a61 * C1_6 - a16 * C1_1
+    v42 = a42 * C1_4 - a24 * C1_2
+
+    eqs = [
+        ET_C1 ~ C1_1+C1_2+C1_3+C1_4+C1_5+C1_6+C1_7,
+        D(C1_1) ~ v61 - v12,
+        D(C1_3) ~ v23 - v34,
+        D(C1_4) ~ v34 - v47 - v42,
+        D(C1_7) ~ v47 - v75,
+        D(C1_5) ~ v75 - v56,
+        D(C1_6) ~ v56 - v61,
+        vQC1 ~ -0.5v47,
+        vROSC1 ~ v42,
+        vNADHC1 ~ -v23,
+        TN_C1 ~ -vNADHC1 / ET_C1,
+        ROSshunt_C1 ~ -vROSC1 / vNADHC1,
+    ]
+    return ODESystem(eqs, t; name)
+end
+
 @parameters begin
-    Qn = 3.0mM
+    Q_n = 3.0mM
     QH2_n = 0.3mM
     nad = 500μM
     nadh = 500μM
     dpsi = 150mV
 end
 
-sys = get_c1_sys(; Qn, QH2_n, nad, nadh, dpsi) |> structural_simplify
+markevich = c1_markevich(; Q_n, QH2_n, nad, nadh, dpsi) |> structural_simplify
 
-tend = 100.0ms
-prob = ODEProblem(sys, [], tend, [sys.nad => 10μM, sys.nadh => 990μM])
+gauthier = c1_gauthier(; Q_n, QH2_n, nad, nadh, dpsi) |> structural_simplify
 
-sol = solve(prob)
+tend = 1000.0ms
+prob_m = ODEProblem(markevich, [], tend, [])
+prob_g = ODEProblem(gauthier, [], tend, [])
 
-sol[sys.fFMNH_C1][end]
-sol[sys.fFMNsq_C1][end]
-sol[sys.KEQ3_C1][end]
-sol[sys.SQ_C1][end]
-sol[sys.vROS_C1][end]
-sol[sys.vNADH_C1][end]
+sol_m = solve(prob_m)
+sol_g = solve(prob_g)
 
-@unpack vQ_C1, vQH2_C1, vROS_C1, vNADH_C1 = sys
-plot(sol, idxs=[vQ_C1, vQH2_C1, vROS_C1, vNADH_C1], tspan=(20, 40))
+sol_m[markevich.TN_C1][end]
+sol_g[gauthier.TN_C1][end]
+
+sol_m[markevich.vROSIf][end]
+sol_m[markevich.vROSIq][end]
+sol_m[markevich.ROSshunt_C1][end]
+sol_g[gauthier.ROSshunt_C1][end]
+
+plot(sol_m, idxs=[markevich.vQ_C1, markevich.vQH2_C1, markevich.vROS_C1, markevich.vNADH_C1], tspan=(20, 40))
+
+plot(sol_g)
