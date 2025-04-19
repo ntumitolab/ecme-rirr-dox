8b961fd76ebefebac37127573f5df7b0bb6983a0
Wen Wei Tseng <sosiristseng@gmail.com>
2025-04-19 17:27:11 +0800

    new form

--- docs/enzflux.jl
+++ docs/enzflux.jl
@@ -54,32 +54,24 @@ for k in (I1, I2, I3, I4, I5, I6, I7)
 end
 
 # ## Test complex I model
-@variables a12 a21 a23 a32 a34 a43 a45 a54 a51 a15 a16 a61 a47 a74
-@variables FMN FMN_NAD FMN_NADH FMNH_NAD FMNH_NADH FMNH FMNsq
+@variables a12 a21 a23 a32 a34 a43 a41 a14
+@variables I1 I2 I3 I4
 
 eqs = let
-    v12 = FMN * a12 - FMN_NADH * a21
-    v23 = FMN_NADH * a23 - FMNH_NAD * a32
-    v34 = FMNH_NAD * a34 - FMNH * a43
-    v47 = FMNH * a47 - FMNH_NADH * a74
-    v45 = FMNH * a45 - FMNsq * a54
-    v51 = FMNsq * a51 - FMN * a15
-    v61 = FMN_NAD * a61 - FMN * a16
-    d1 = -v12 + v61 + v51
+    v12 = I1 * a12 - I2 * a21
+    v23 = I2 * a23 - I3 * a32
+    v34 = I3 * a34 - I4 * a43
+    v41 = I4 * a41 - I1 * a14
+    d1 = v41 - v12
     d2 = v12 - v23
     d3 = v23 - v34
-    d4 = v34 - v47 - v45
-    d7 = v47
-    d5 = v45 - v51
-    d6 = -v61
-    @assert isequal(d1 + d2 + d3 + d4 + d5 + d6 + d7, 0)
-    [d1, d2, d3, d4, d5, d6, FMN + FMN_NADH + FMNH_NAD + FMNH + FMNsq + FMN_NAD + FMNH_NADH - 1]
+    d4 = v34 - v41
+    @assert isequal(d1 + d2 + d3 + d4, 0)
+    [d1, d2, d3, I1 + I2 + I3 + I4 - 1]
 end
 
-@time sol = Symbolics.symbolic_solve(eqs, [FMN, FMN_NADH, FMNH_NAD, FMNH, FMNsq, FMN_NAD, FMNH_NADH])[1]
+@time sol = Symbolics.symbolic_solve(eqs, [I1, I2, I3, I4])[1]
 
-for k in (FMN, FMN_NADH, FMNH_NAD, FMNH, FMNsq, FMN_NAD, FMNH_NADH)
+for k in (I1, I2, I3, I4)
     println(k, " = ", numerator(sol[k]))
 end
-
-Symbolics.factors(a21*a32*a43*a51*a61*a74 + a21*a32*a43*a54*a61*a74 + a21*a32*a45*a51*a61*a74)
