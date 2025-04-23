a80d6127f5d52a6e1da395cc201f98e6004842e8
Wen Wei Tseng <sosiristseng@gmail.com>
2025-04-23 16:50:48 +0800

    Why no response to DOX?

--- docs/dox.jl
+++ docs/dox.jl
@@ -13,14 +13,14 @@ Plots.default(lw=1.5, size=(600, 600))
 tend = 1000.0second
 bcl = 1second
 @named sys = build_model(; bcl, tend)
-@unpack DOX, ρC4, ρC3 = sys
+@unpack DOX, rhoC4, rhoC3 = sys
 sts = unknowns(sys)
 u0 = build_u0(sys)
 alg = KenCarp47()
 opts = (; reltol=1e-6, abstol=1e-6, progress=true)
 
 # The collapse of MMP is between 295uM and 296uM of DOX
-doxrange = 294μM:1μM:297μM
+doxrange = 400μM:10μM:500μM
 prob = ODEProblem(sys, u0, tend)
 prob_func = (prob, i, repeat) -> remake(prob, p=[DOX => doxrange[i]])
 eprob = EnsembleProblem(prob; prob_func)
@@ -84,74 +84,4 @@ plot(sim[end], idxs=[Q_n + Q_p, SQn, QH2_n+QH2_p, SQp], title="Q cycle ", legend
 @unpack cit, isoc, oaa, akg, scoa, suc, fum, mal = sys
 plot(sim[end], idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal], title="TCA cycle ", legend=:right) |> PNG
 
-#---
-prob0 = ODEProblem(sys, u0, tend, [DOX => 296μM])
-prob1 = ODEProblem(sys, u0, tend, [DOX => 296μM, ρC4 => 500μM])
-prob2 = ODEProblem(sys, u0, tend, [DOX => 296μM, ρC3 => 500μM])
-@time sol0 = solve(prob0, alg; opts...)
-@time sol1 = solve(prob1, alg; opts...)
-@time sol2 = solve(prob2, alg; opts...)
-
-#---
-s = sys.t / 1000
-@unpack vm, atp_i, adp_i, dpsi = sys
-plot(sol0, idxs=(s, vm), lab="DOX=296", title="PM potential") |> PNG
-
-#---
-plot(sol1, idxs=(s, vm), lab="DOX=296, ρC4=500", title="PM potential") |> PNG
-
-#---
-plot(sol2, idxs=(s, vm), lab="DOX=296, ρC3=500", title="PM potential") |> PNG
-
-#---
-i = (s, [atp_i / adp_i])
-fig = plot(sol0, idxs=i, label="DOX=296", title="ATP:ADP")
-plot!(fig, sol1, idxs=i, label="C4 500uM")
-plot!(fig, sol2, idxs=i, label="C3 500uM")
-fig |> PNG
-
-#---
-i = (s, sys.dpsi)
-fig = plot(sol0, idxs=i, label="DOX=296", title="MMP")
-plot!(fig, sol1, idxs=i, label="C4 500uM")
-plot!(fig, sol2, idxs=i, label="C3 500uM")
-fig |> PNG
-
-#---
-idxs = (sys.t/1000, sys.vO2 + sys.vROS)
-fig = plot(sol0, idxs=idxs, label="DOX=296", title="Oxygen consumption")
-plot!(fig, sol1, idxs=idxs, label="C4 500")
-plot!(fig, sol2, idxs=idxs, label="C3 500", xlabel="Time (s)", ylabel="vO2 (μM/ms)")
-fig |> PNG
-
-# O2 shunt
-idxs = (sys.t/1000, sys.vROS / (sys.vO2 + sys.vROS))
-fig = plot(sol0, idxs=idxs, label="DOX=296", title="ROS shunt fraction")
-plot!(fig, sol1, idxs=idxs, label="C4 500")
-plot!(fig, sol2, idxs=idxs, label="C3 500", xlabel="Time (s)", legend=:right)
-fig |> PNG
-
-# Q cycle : reduced Q pool (QH2 accumulation)
-@unpack Q_n, SQn, QH2_n, QH2_p, SQp, Q_p, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
-plot(sol0, idxs=[Q_n + Q_p, SQn, QH2_n+QH2_p, SQp], title="Q cycle (DOX=296uM)", legend=:right) |> PNG
-
-# Succinate accumulation and CAC slowed down.
-@unpack cit, isoc, oaa, akg, scoa, suc, fum, mal= sys
-plot(sol0, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal], legend=:right, title="CAC metabolites") |> PNG
-
-# TCA cycle flux
-@unpack vCS, vACO, vIDH, vKGDH, vSL, vFH, vMDH, vAAT, vSDH = sys
-plot(sol0, idxs=[vCS, vACO, vIDH, vKGDH, vFH, vMDH, vAAT, vSDH], legend=:right, title="CAC flux", tspan=(100second, 1000second)) |> PNG
-
-# ROS production from complex I and complex III
-plot(sol0, idxs=[sys.vROSC1, sys.vROSC3], legend=:right, title="Superoxide generation") |> PNG
-
-# Reverse electron transport in complex I?
-plot(sol0, idxs=[sys.vQC1], legend=:right, title="Complex I rates") |> PNG
-
-# NADH production from CAC decreased
-idxs = (sys.t/1000, sys.nadh_m)
-fig = plot(sol0, idxs=idxs, label="DOX=296", title="NADH (mito)")
-plot!(fig, sol1, idxs=idxs, label="C4 500")
-plot!(fig, sol2, idxs=idxs, label="C3 500", xlabel="Time (s)", legend=:right)
-fig |> PNG
+plot(sim[end], idxs=[sys.nadh_m], legend=:right) |> PNG
