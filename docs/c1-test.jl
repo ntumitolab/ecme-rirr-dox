b630df23871f5a7b7c4303342228c2747616ee8d
Wen Wei Tseng <sosiristseng@gmail.com>
2025-05-20 17:22:36 +0800

    wip

--- docs/c1-test.jl
+++ docs/c1-test.jl
@@ -274,25 +274,25 @@ function c1q(; name=:c1q,
         Em_N2 = -80mV               ## FeS N2 redox potential
         Em_Q_SQ_C1 = -300mV         ## -213mV in Markevich, 2015
         Em_SQ_QH2_C1 = +500mV       ## 800mV in Markevich, 2015
-        KI_NADH_C1 = 50μM
+        KI_NADH_C1 = 150μM
         KI_NAD_C1 = 1000μM
         ## NADH + FMN = NAD+ + FMNH-
-        KEQ2_C1 = exp(2iVT * (Em_FMN_FMNH - Em_NAD))
+        KEQ_NADH_FMN = exp(2iVT * (Em_FMN_FMNH - Em_NAD))
+        ## FMNH- + Q = FMNsq + SQ
+        rKEQ_FMNH_Q = exp(-iVT * (Em_Q_SQ_C1 - Em_FMNsq_FMNH))
+        ## FMNsq + Q = FMN + SQ + H+
+        rKEQ_FMNsq_Q = exp(-iVT * (Em_Q_SQ_C1 - Em_FMN_FMNsq))
+
         ## FMNH- + N3 = FMNH + N3-
         KEQ_FMNH_N3 = exp(iVT * (Em_N3 - Em_FMNsq_FMNH))
-        ## 2FMNH = FMN + FMNH- + H+
+        ## Dismutation: 2FMNH = FMN + FMNH- + H+
         rKEQ_FMNsq_Dis = exp(-iVT * (Em_FMNsq_FMNH - Em_FMN_FMNsq))
-        ## ET from N3 to N2
+        ## ET from N3 to Q
         kf7_C1 = 10000Hz / μM
         rKEQ7_C1 = exp(-iVT * (Em_N3 - Em_Q_SQ_C1))
         ## Q binding
         kf8_C1 = 10Hz / μM
         rKEQ8_C1 = 10μM
-        ## ET from N2 to Q
-        kf9_C1 = 4E5Hz / μM
-        KEQ9_C1 = exp(iVT * (Em_Q_SQ_C1 - Em_N2))
-        ## ET from FMNsq to N3
-        rKEQ11_C1 = exp(-iVT * (Em_N3 - Em_FMN_FMNsq))
         ## ET from N2 to SQ
         kf13_C1 = 2.7e6Hz / μM
         ## QH2 unbinding
@@ -334,9 +334,9 @@ function c1q(; name=:c1q,
     fhm = h_m * inv(1E-7Molar)
     ## Weights in the flavin site
     wFMN = 1
-    wFMN_NAD = wFMN * KI_NAD_C1 / (nad + KI_NAD_C1)
-    wFMNH = wFMN * (nadh / nad) * KEQ2_C1
-    wFMNH_NADH = wFMNH * nadh / (nadh + KI_NADH_C1)
+    wFMN_NAD = wFMN * nad / KI_NAD_C1
+    wFMNH = wFMN * (nadh / nad) * KEQ_NADH_FMN
+    wFMNH_NADH = wFMNH * nadh / KI_NADH_C1
     wFMNsq = NaNMath.sqrt(wFMN * wFMNH * rKEQ_FMNsq_Dis * fhm)
     denf = wFMN + wFMN_NAD + wFMNH + wFMNH_NADH + wFMNsq
 
@@ -393,7 +393,7 @@ qsys = c1q(; Q_n, QH2_n, nad, nadh, dpsi) |> structural_simplify
 markevich = c1_markevich_full(; Q_n, QH2_n, nad, nadh, dpsi) |> structural_simplify
 gauthier = c1_gauthier(; Q_n, QH2_n, nad, nadh, dpsi) |> structural_simplify
 
-prob_q = SteadyStateProblem(qsys, [qsys.ET_C1 => 3μM, qsys.kf7_C1 => 1000Hz / μM, qsys.kf8_C1 => 10Hz / μM, qsys.kf13_C1 => 10000Hz / μM, qsys.kf14_C1 => 1000Hz])
+prob_q = SteadyStateProblem(qsys, [qsys.ET_C1 => 3μM, qsys.kf7_C1 => 1000Hz / μM, qsys.kf8_C1 => 10Hz / μM, qsys.kf13_C1 => 1e6Hz / μM, qsys.kf14_C1 => 1000Hz, qsys.kf16_C1 => 0.0015Hz / μM, qsys.kf17_C1 => 0.0002Hz])
 prob_m = SteadyStateProblem(markevich, [markevich.ET_C1 => 17μM, markevich.kf16_C1 => 0.001Hz / μM, markevich.kf17_C1 => 0.001Hz / μM / 20])
 prob_g = SteadyStateProblem(gauthier, [])
 alg = DynamicSS(Rodas5P())
@@ -467,8 +467,16 @@ ys = [extract(sim_g, gauthier.vROS_C1) extract(sim_m, markevich.vROS_C1) extract
 plot(xs, ys, xlabel="NADH (μM)", ylabel="ROS production", label=["Gauthier" "Markevich" "IQ"])
 
 #---
-ys = stack(extract.(Ref(sim_q), [qsys.N2_C1, qsys.N2Q_C1, qsys.N2r_C1, qsys.N2rQ_C1, qsys.N2QH_C1, qsys.N2rQH_C1]), dims=2)
-plot(xs, ys, xlabel="NADH (μM)", ylabel="Concentration", label=["N2_C1" "N2Q_C1" "N2r_C1" "N2rQ_C1" "N2QH_C1" "N2rQH_C1"], legend=:right)
+ys = stack(extract.(Ref(sim_m), [markevich.Q_C1, markevich.SQ_C1, markevich.QH2_C1]), dims=2)
+plot(xs, ys, xlabel="NADH (μM)", ylabel="Concentration", label=["Q_C1" "SQ_C1" "QH2_C1"], legend=:left, title="M model")
+
+#---
+ys = stack(extract.(Ref(sim_m), [markevich.FMN, markevich.FMNsq, markevich.FMNH, markevich.FMN_NAD, qsys.FMNH_NADH]), dims=2)
+plot(xs, ys, xlabel="NADH (μM)", ylabel="Concentration", label=["FMN" "FMNsq" "FMNH" "FMN_NAD" "FMNH_NADH"], legend=:right, title="M model")
+
+#---
+ys = stack(extract.(Ref(sim_q), [qsys.IqQ, qsys.IqSQ, qsys.IqQH2]), dims=2)
+plot(xs, ys, xlabel="NADH (μM)", ylabel="Concentration", label=["IqQ" "IqSQ" "IqQH2"], legend=:left, title="Iq model")
 
 #---
 ys = stack(extract.(Ref(sim_q), [qsys.FMN, qsys.FMNsq, qsys.FMNH, qsys.FMN_NAD, qsys.FMNH_NADH]), dims=2)
@@ -506,6 +514,10 @@ plot(xs, ys, xlabel="QH2 (μM)", ylabel="Q rate (μM/ms)", label=["Gauthier" "Ma
 xs = qh2range
 ys = [extract(sim_g, gauthier.vROS_C1) extract(sim_m, markevich.vROS_C1) extract(sim_q, qsys.vROS_C1)]
 plot(xs, ys, xlabel="QH2 (μM)", ylabel="ROS production", label=["Gauthier" "Markevich" "IQ"])
+
+#---
+ys = stack(extract.(Ref(sim_q), [qsys.IqQ, qsys.IqSQ, qsys.IqQH2]), dims=2)
+plot(xs, ys, xlabel="QH2 (μM)", ylabel="Concentration", label=["IqQ" "IqSQ" "IqQH2"], legend=:left)
 
 #---
 @unpack C1_1, C1_2, C1_3, C1_4, C1_5, C1_6, C1_7 = gauthier
