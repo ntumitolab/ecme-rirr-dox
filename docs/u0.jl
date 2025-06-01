1dcee93f55b027d04c8db417372ba61b157611f6
Wen Wei Tseng <sosiristseng@gmail.com>
2025-06-01 17:39:28 +0800

    callbacks

--- docs/u0.jl
+++ docs/u0.jl
@@ -4,19 +4,19 @@ using ProgressLogging
 using OrdinaryDiffEq
 using ModelingToolkit
 using ECMEDox
-using ECMEDox: second, mM, Hz, μM
+using ECMEDox: second, mM, Hz, μM, build_stim_callbacks
 using Plots
 using DisplayAs: PNG
 
 tend = 1000.0second
 bcl = 1.0second
-@named sys = build_model(; bcl, tend)
-discrete_events(sys)[1]
+@named sys = build_model(; tend, bcl=0)
 u0 = build_u0(sys)
 sts = unknowns(sys)
 alg = KenCarp47()
-prob = ODEProblem(sys, [u0; sys.KCAT_IDH => 50Hz], tend)
-@time sol = solve(prob, alg; reltol=1e-6, abstol=1e-6, progress=true)
+prob = ODEProblem(sys, u0, tend)
+cb = build_stim_callbacks(sys.iStim, tend)
+@time sol = solve(prob, alg; reltol=1e-6, abstol=1e-6, progress=true, callback=cb)
 
 for i in sts
     istr = replace(string(i), "(t)" => "")
