62afbaebfbf3ccf71b9313fb5ae428671b12889a
Wen Wei Tseng <sosiristseng@gmail.com>
2025-06-23 20:58:39 +0800

    callback fixed

--- docs/u0.jl
+++ docs/u0.jl
@@ -10,19 +10,21 @@ using DisplayAs: PNG
 
 tend = 1000.0second
 bcl = 1.0second
-@named sys = build_model(; tend, bcl=0)
+@named sys = build_model(; tend, bcl)
 u0 = build_u0(sys)
 sts = unknowns(sys)
 alg = KenCarp47()
 prob = ODEProblem(sys, [u0; sys.kf16_C1 => 2Hz / μM; sys.kf17_C1 => 0.02Hz / μM; sys.K010_C3 => 3Hz / mM; sys.KCAT_IDH => 43Hz], tend)
 
-cb = build_stim_callbacks(sys.iStim, tend)
-@time sol = solve(prob, alg; reltol=1e-6, abstol=1e-6, progress=true, callback=cb)
+@time sol = solve(prob, alg; reltol=1e-6, abstol=1e-6, progress=true)
 
 for i in sts
     istr = replace(string(i), "(t)" => "")
     println("sys.", istr, " => ", sol[i][end], ",")
 end
+
+#---
+plot(sol, idxs=sys.vm, legend=:right, tspan=(900second, 901second)) |> PNG
 
 # Citric acid cycle metabolites
 # Dominated by citrate and isocitrate
@@ -33,9 +35,6 @@ plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal], legend=:right, title=
 @unpack Q_n, SQn, QH2_n, QH2_p, Q_p, SQp, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
 pl_q = plot(sol, idxs=[Q_n + Q_p, SQn, QH2_n + QH2_p, SQp], title="Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
 
-
-#---
-sol[sys.SQp]
 #---
 pl_q = plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle (downstream)", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
 
