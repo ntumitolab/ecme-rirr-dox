6acd243577db8ae9ab299c1012483087d4acd31a
Wen Wei Tseng <sosiristseng@gmail.com>
2025-06-23 17:21:05 +0800

    wip

--- docs/c3-test.jl
+++ docs/c3-test.jl
@@ -333,6 +333,155 @@ function c3_semireverse(;
     return System(eqs, t; name)
 end
 
+## Complex III repulsion model
+## Reduced heme bL stops QH2 oxidation at Qo site
+## Unstable SQp (-300mV)
+function c3_repulsion(;
+    dpsi=150mV,
+    MT_PROT=1,
+    O2=6μM,
+    sox_m=0.001μM,
+    h_i=exp10(-7) * Molar,
+    h_m=exp10(-7.6) * Molar,
+    ANTIMYCIN_BLOCK=0,
+    MYXOTHIAZOLE_BLOCK=0,
+    UQ = 3600μM,
+    UQH2 = 400μM,
+    cytc_ox = 208μM,
+    cytc_rd = 325μM - cytc_ox,
+    name = :c3_repulse
+    )
+
+    @parameters begin
+        rhoC3 = 325μM    ## Complex III activity
+        Q_T = 4mM        ## Total CoQ pool
+        EmQp = +60mV
+        EmSQp_QH2p = +290mV
+        EmQp_SQp = 2EmQp - EmSQp_QH2p
+        EmQn_SQn = +50mV
+        EmSQn_QH2n = +150mV
+        EmbL_bHo = -40mV
+        EmbL_bHr = EmbL_bHo - 60mV
+        EmbH_bLo = +20mV
+        EmbH_bLr = EmbH_bLo - 60mV
+        EmFeS = +280mV
+        Emcytc1 = +240mV
+        EmO2 = -160mV
+        Emcytc = +260mV
+        K03_C3 = 1666.63Hz / mM
+        KEQ3_C3 = exp(iVT * (EmFeS - EmSQp_QH2p))
+        K04_C3 = 50.67Hz / mM
+        KEQ4_OX_C3 = exp(iVT * (EmbL_bHo - EmQp_SQp))
+        KEQ4_RD_C3 = exp(iVT * (EmbL_bHr - EmQp_SQp))
+        KD_Q = 22000Hz
+        K06_C3 = 10000Hz # from 166.67Hz
+        KEQ6_C3 = exp(iVT * (EmbH_bLo - EmbL_bHo))
+        K07_OX_C3 = 13.33Hz / mM
+        K07_RD_C3 = 1.67Hz / mM
+        KEQ7_OX_C3 = exp(iVT * (EmQn_SQn - EmbH_bLo))
+        KEQ7_RD_C3 = exp(iVT * (EmQn_SQn - EmbH_bLr))
+        K08_OX_C3 = 83.33Hz / mM
+        K08_RD_C3 = 8.33Hz / mM
+        KEQ8_OX_C3 = exp(iVT * (EmSQn_QH2n - EmbH_bLo))
+        KEQ8_RD_C3 = exp(iVT * (EmSQn_QH2n - EmbH_bLr))
+        K09_C3 = 832.48Hz / mM
+        KEQ9_C3 = exp(iVT * (Emcytc1 - EmFeS))
+        K010_C3 = 28.33Hz / mM
+        KEQ10_C3 = exp(iVT * (EmO2 - EmQp_SQp))
+        K33_C3 = 2469.13Hz / mM
+        KEQ33_C3 = exp(iVT * (Emcytc - Emcytc1))
+    end
+
+    ## complex III inhibition by DOX and antimycin
+    C3_CONC = rhoC3 * MT_PROT
+
+    @variables begin
+        Q_n(t)
+        QH2_n(t)
+        QH2_p(t)
+        Q_p(t)
+        SQp(t) = 0
+        SQn(t) = 0
+        fes_ox(t) = C3_CONC
+        fes_rd(t) ## Conserved
+        cytc1_ox(t) = C3_CONC
+        cytc1_rd(t) ## Conserved
+        b_00(t) = C3_CONC
+        b_10(t) = 0
+        b_01(t) = 0
+        b_11(t) ## Conserved
+        fracbLrd(t)
+        fracbHrd(t)
+        vROSC3(t)
+        vHresC3(t)
+    end
+
+    ## Split of electrical potentials
+    δ₁_C3 = 0.5
+    δ₂_C3 = 0.5
+    δ₃_C3 = 0.5
+    ## Split of the electrical distance across the IMM
+    α_C3 = 0.25
+    β_C3 = 0.5
+    γ_C3 = 0.25
+    fHi = h_i * inv(1E-7Molar)
+    fHm = h_m * inv(1E-7Molar)
+    ## QH2 + FeS = Q- + FeS- + H+
+    Qo_avail = (C3_CONC - SQp) / C3_CONC * (1 - fracbLrd) * (1 - MYXOTHIAZOLE_BLOCK)
+    v3 = K03_C3 * (KEQ3_C3 * Qo_avail * fes_ox * QH2_p - fes_rd * SQp * fHi^2)
+    ## v4: Q- + bL = Qp + bL- + H+
+    el4 = exp(-iVT * α_C3 * δ₁_C3 * dpsi)
+    er4 = exp(iVT * α_C3 * (1 - δ₁_C3) * dpsi)
+    v4_ox = K04_C3 * (KEQ4_OX_C3 * SQp * el4 * b_00 - Q_p * er4 * b_10)
+    v4_rd = K04_C3 * (KEQ4_RD_C3 * SQp * el4 * b_01 - Q_p * er4 * b_11)
+    ## v5 = Q diffusion (p-side -> n-side)
+    v5 = KD_Q * (Q_p - Q_n)
+    ## v6 = bL to bH
+    el6 = exp(-iVT * β_C3 * δ₂_C3 * dpsi)
+    er6 = exp(iVT * β_C3 * (1 - δ₂_C3) * dpsi)
+    v6 = K06_C3 * (KEQ6_C3 * b_10 * el6 - b_01 * er6)
+    ## v7 = bH to Qn; v8: bH to SQn
+    Qi_avail = (C3_CONC - SQn) / C3_CONC * (1 - ANTIMYCIN_BLOCK)
+    el7 = exp(-iVT * γ_C3 * δ₃_C3 * dpsi)
+    er7 = exp(iVT * γ_C3 * (1 - δ₃_C3) * dpsi)
+    qn = Q_n * Qi_avail
+    qh2n = QH2_n * Qi_avail
+    v7_ox = K07_OX_C3 * (KEQ7_OX_C3 * b_01 * qn * el7 - b_00 * SQn * er7)
+    v7_rd = K07_RD_C3 * (KEQ7_RD_C3 * b_11 * qn * el7 - b_10 * SQn * er7)
+    v8_ox = K08_OX_C3 * (KEQ8_OX_C3 * b_01 * SQn * fHm^2 * el7 - b_00 * qh2n * er7)
+    v8_rd = K08_RD_C3 * (KEQ8_RD_C3 * b_11 * SQn * fHm^2 * el7 - b_10 * qh2n * er7)
+    ## v9 = fes -> cytc1
+    v9 = K09_C3 * (KEQ9_C3 * fes_rd * cytc1_ox - fes_ox * cytc1_rd)
+    ## v10: SQp + O2 -> O2- + Q
+    v10 = K010_C3 * (KEQ10_C3 * O2 * SQp - sox_m * Q_p)
+    ## cytc1_2+  + cytc_3+ = cytc1_3+  + cytc_2+
+    v33 = K33_C3 * (KEQ33_C3 * cytc1_rd * cytc_ox - cytc1_ox * cytc_rd)
+
+    eqs = [
+        C3_CONC ~ b_00 + b_01 + b_10 + b_11,
+        C3_CONC ~ fes_ox + fes_rd,
+        C3_CONC ~ cytc1_ox + cytc1_rd,
+        Q_n ~ 0.5 * UQ,
+        Q_p ~ 0.5 * UQ,
+        QH2_n ~ 0.5 * UQH2,
+        QH2_p ~ 0.5 * UQH2,
+        fracbLrd ~ (b_10 + b_11) / C3_CONC,
+        fracbHrd ~ (b_01 + b_11) / C3_CONC,
+        ## D(UQH2) ~ dQH2n + dQH2p,
+        D(SQn) ~ v7_ox + v7_rd - v8_ox - v8_rd,
+        D(SQp) ~ v3 - v10 - v4_ox - v4_rd,
+        D(b_00) ~ v7_ox + v8_ox - v4_ox,
+        D(b_10) ~ v4_ox + v7_rd + v8_rd - v6,
+        D(b_01) ~ v6 - v4_rd - v7_ox - v8_ox,
+        ## D(b_11) = v4_rd - v7_rd - v8_rd
+        D(fes_ox) ~ v9 - v3,
+        D(cytc1_ox) ~ v33 - v9,
+        vHresC3 ~ v6,
+        vROSC3 ~ v10,
+    ]
+    return System(eqs, t; name)
+end
+
 #---
 @parameters begin
     UQ = 3600μM
@@ -345,11 +494,11 @@ end
 
 #---
 gsys = c3_gauthier(;dpsi, cytc_ox, cytc_rd, UQ, UQH2, sox_m) |> mtkcompile
-fsys = c3_gauthier(;dpsi, cytc_ox, cytc_rd, UQ, UQH2, sox_m, name=:fast) |> mtkcompile
+nsys = c3_repulsion(;dpsi, cytc_ox, cytc_rd, UQ, UQH2, sox_m) |> mtkcompile
 rsys = c3_semireverse(;dpsi, cytc_ox, cytc_rd, UQ, UQH2, sox_m) |> mtkcompile
 #---
 prob_g = SteadyStateProblem(gsys, [])
-prob_f = SteadyStateProblem(fsys, [fsys.K06_C3 => 10000Hz])
+prob_n = SteadyStateProblem(nsys, [nsys.K03_C3 => 1666.63Hz / mM, nsys.])
 prob_r = SteadyStateProblem(rsys, [rsys.K010_C3 => 4Hz / mM, rsys.K011_C3 => 100Hz / mM, rsys.K04_C3 => 50Hz / mM])
 alg = DynamicSS(Rodas5P())
 ealg = EnsembleThreads()
@@ -363,22 +512,41 @@ alter_dpsi = (prob, i, repeat) -> begin
 end
 
 eprob_g = EnsembleProblem(prob_g; prob_func=alter_dpsi)
-eprob_f = EnsembleProblem(prob_f; prob_func=alter_dpsi)
+eprob_n = EnsembleProblem(prob_n; prob_func=alter_dpsi)
 eprob_r = EnsembleProblem(prob_r; prob_func=alter_dpsi)
 @time sim_g = solve(eprob_g, alg, ealg; trajectories=length(dpsirange), abstol=1e-8, reltol=1e-8)
-@time sim_f = solve(eprob_f, alg, ealg; trajectories=length(dpsirange), abstol=1e-8, reltol=1e-8)
+@time sim_n = solve(eprob_n, alg, ealg; trajectories=length(dpsirange), abstol=1e-8, reltol=1e-8)
 @time sim_r = solve(eprob_r, alg, ealg; trajectories=length(dpsirange), abstol=1e-8, reltol=1e-8)
 
 xs = dpsirange
-ys = [extract(sim_g, gsys.vHresC3) extract(sim_r, rsys.vHresC3) extract(sim_f, fsys.vHresC3)]
-plot(xs, ys, xlabel="MMP (mV)", ylabel="Resp. Rate (mM/s)", label=["G" "R" "F"])
+ys = [extract(sim_g, gsys.vHresC3) extract(sim_r, rsys.vHresC3) extract(sim_n, nsys.vHresC3)]
+plot(xs, ys, xlabel="MMP (mV)", ylabel="Resp. Rate (mM/s)", label=["G" "R" "New"])
 
 #---
-ys = [extract(sim_g, gsys.fracbLrd) extract(sim_f, fsys.fracbLrd) extract(sim_g, gsys.fracbHrd) extract(sim_f, fsys.fracbHrd)]
+plot(xs, extract(sim_n, nsys.b_00), label="bL(ox)-bH(ox)", title="New model")
+plot!(xs, extract(sim_n, nsys.b_10), label="bL(rd)-bH(ox)")
+plot!(xs, extract(sim_n, nsys.b_01), label="bL(ox)-bH(rd)")
+pl1 = plot!(xs, extract(sim_n, nsys.b_11), label="bL(rd)-bH(rd)")
+
+#---
+plot(xs, extract(sim_g, gsys.cytb_1), label="bL(ox)-bH(ox)", title = "G model")
+plot!(xs, extract(sim_g, gsys.cytb_2), label="bL(rd)-bH(ox)")
+plot!(xs, extract(sim_g, gsys.cytb_3), label="bL(ox)-bH(rd)")
+pl2 = plot!(xs, extract(sim_g, gsys.cytb_4), label="bL(rd)-bH(rd)")
+
+#---
+plot(pl1, pl2)
+
+
+ys = [extract(sim_n, gsys.vHresC3) extract(sim_n, rsys.vHresC3) extract(sim_n, nsys.vHresC3)]
+
+
+#---
+ys = [extract(sim_g, gsys.fracbLrd) extract(sim_n, nsys.fracbLrd) extract(sim_g, gsys.fracbHrd) extract(sim_n, nsys.fracbHrd)]
 plot(xs, ys, xlabel="MMP (mV)", ylabel="Reduced fraction", label=["G (bL)" "F (bL)" "G (bH)" "F (bH)"], line=[:solid :dash :solid :dash])
 
 # ROS generation rate: 0.005 ~ 0.020 mM/s
-ys = [extract(sim_g, gsys.vROSC3) extract(sim_r, rsys.vROSC3) extract(sim_f, fsys.vROSC3)]
+ys = [extract(sim_g, gsys.vROSC3) extract(sim_r, rsys.vROSC3) extract(sim_n, nsys.vROSC3)]
 plot(xs, ys, xlabel="MMP (mV)", ylabel="ROS Rate (mM/s)", label=["G" "R" "F"])
 
 # ## Varying UQH2
@@ -390,23 +558,23 @@ alter_qh2 = (prob, i, repeat) -> begin
 end
 
 eprob_g = EnsembleProblem(prob_g; prob_func=alter_qh2)
-eprob_f = EnsembleProblem(prob_f; prob_func=alter_qh2)
+eprob_n = EnsembleProblem(prob_n; prob_func=alter_qh2)
 eprob_r = EnsembleProblem(prob_r; prob_func=alter_qh2)
 @time sim_g = solve(eprob_g, alg, ealg; trajectories=length(qh2range), abstol=1e-8, reltol=1e-8)
-@time sim_f = solve(eprob_f, alg, ealg; trajectories=length(qh2range), abstol=1e-8, reltol=1e-8)
+@time sim_n = solve(eprob_n, alg, ealg; trajectories=length(qh2range), abstol=1e-8, reltol=1e-8)
 @time sim_r = solve(eprob_r, alg, ealg; trajectories=length(qh2range), abstol=1e-8, reltol=1e-8)
 
 #---
 xs = qh2range ./ 4000μM .* 100
-ys = [extract(sim_g, gsys.vHresC3) extract(sim_r, rsys.vHresC3) extract(sim_f, fsys.vHresC3)]
+ys = [extract(sim_g, gsys.vHresC3) extract(sim_r, rsys.vHresC3) extract(sim_n, nsys.vHresC3)]
 plot(xs, ys, xlabel="QH2 (%)", ylabel="Resp. Rate (mM/s)", label=["G" "R" "F"])
 
 #---
-ys = [extract(sim_g, gsys.fracbLrd) extract(sim_f, fsys.fracbLrd) extract(sim_g, gsys.fracbHrd) extract(sim_f, fsys.fracbHrd)]
+ys = [extract(sim_g, gsys.fracbLrd) extract(sim_n, nsys.fracbLrd) extract(sim_g, gsys.fracbHrd) extract(sim_n, nsys.fracbHrd)]
 plot(xs, ys, xlabel="QH2 (%)", ylabel="Reduced fraction", label=["G (bL)" "F (bL)" "G (bH)" "F (bH)"], line=[:solid :dash :solid :dash])
 
 #---
-ys = [extract(sim_g, gsys.vROSC3) extract(sim_r, rsys.vROSC3) extract(sim_f, fsys.vROSC3)]
+ys = [extract(sim_g, gsys.vROSC3) extract(sim_r, rsys.vROSC3) extract(sim_n, nsys.vROSC3)]
 plot(xs, ys, xlabel="QH2 (%)", ylabel="ROS Rate (mM/s)", label=["G" "R" "F"])
 
 #---
