63ffe72d438669287a0f4ff0ff355f5bb40743cf
Wen Wei Tseng <sosiristseng@gmail.com>
2025-06-29 17:19:08 +0800

    test code

--- test.jl
+++ test.jl
@@ -5,17 +5,23 @@ using DiffEqCallbacks
 using Plots
 
 @parameters iStim(t)
-@variables x(t) v_istim(t)
-
-@named sys = ODESystem([D(x) ~ -x + iStim], t)
-sys = structural_simplify(sys)
+@variables x(t) v_istim(t) rhs(t)
 
 stimulate! = (integrator) -> begin
     integrator.ps[iStim] = 5
 end
+cb = PresetTimeCallback(1.0, stimulate!)
+
+ev = ModelingToolkit.SymbolicDiscreteCallback(
+    [1.0] => [iStim ~ 5], discrete_parameters = iStim, iv = t)
+
+@mtkcompile sys = ODESystem([D(x) ~ rhs, rhs ~ -x + v_istim, v_istim ~ iStim], t; discrete_events = [ev])
 
 tspan = (0.0, 10.0)
 prob = ODEProblem(sys, [x => 0.0, iStim=>0.0], tspan)
-cb = PresetTimeCallback(1.0, stimulate!)
 
-@time sol = solve(prob; callback=cb)
+@time sol = solve(prob)
+
+ts = 0:0.1:10.0
+ys = sol(ts, idxs=[sys.iStim, sys.x]).u |> stack |> transpose
+plot(ts, ys, label=["iStim" "x"])
