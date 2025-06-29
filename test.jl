150e8639f9874e75743769b9cfc457e4e14ca033
Wen Wei Tseng <sosiristseng@gmail.com>
2025-06-29 22:57:04 +0800

    test code

--- test.jl
+++ test.jl
@@ -15,12 +15,21 @@ cb = PresetTimeCallback(1.0, stimulate!)
 ev = ModelingToolkit.SymbolicDiscreteCallback(
     [1.0] => [iStim ~ 5], discrete_parameters = iStim, iv = t)
 
-@mtkcompile sys = ODESystem([D(x) ~ rhs, rhs ~ -x + v_istim, v_istim ~ iStim], t; discrete_events = [ev])
+@named model = System([D(x) ~ rhs, rhs ~ -x + v_istim, v_istim ~ iStim], t; discrete_events = [ev])
+
+sys = mtkcompile(model)
+
+@named model2 = System(equations(model), t)
+
+sys2 = mtkcompile(model2)
 
 tspan = (0.0, 10.0)
 prob = ODEProblem(sys, [x => 0.0, iStim=>0.0], tspan)
 
 @time sol = solve(prob)
+
+prob2 = ODEProblem(sys2, [x => 0.0, iStim=>0.0], tspan)
+@time sol2 = solve(prob2)
 
 ts = 0:0.1:10.0
 ys = sol(ts, idxs=[sys.iStim, sys.x]).u |> stack |> transpose
