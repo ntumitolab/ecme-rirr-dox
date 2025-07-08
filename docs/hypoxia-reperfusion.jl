578b7a18f566189816ee94f986f82105f6274c97
Wen Wei Tseng <sosiristseng@gmail.com>
2025-07-08 15:45:15 +0800

    Em(N2)=-150mV (#53)
    * Em(N2)=-150mV
    
    * py for jl ver

--- docs/hypoxia-reperfusion.jl
+++ docs/hypoxia-reperfusion.jl
@@ -4,7 +4,7 @@ using OrdinaryDiffEq
 using ModelingToolkit
 using DiffEqCallbacks
 using ECMEDox
-using ECMEDox: second, Hz, μM, nM, build_stim_callbacks
+using ECMEDox: second, Hz, μM, nM, mV
 using Plots
 using DisplayAs: PNG
 Plots.default(lw=1.5)
@@ -16,8 +16,7 @@ bcl = 1.0second
 u0 = build_u0(sys)
 sts = unknowns(sys)
 alg = KenCarp47()
-@unpack O2_o = sys
-prob = ODEProblem(sys, [u0; O2_o => 6nM], tend)
+prob = ODEProblem(sys, [u0; sys.O2_o => 6nM; sys.Em_N2 => -150mV], tend)
 
 reoxygen! = (integrator) -> begin
     integrator.ps[sys.O2_o] = 6μM
@@ -49,39 +48,39 @@ plot(pl_mmp, pl_vm, pl_atp, pl_cac, pl_q, pl_ros, size=(1200, 800)) |> PNG
 
 # MMP
 @unpack vm, dpsi, atp_i, adp_i = sys
-pl_mmp = plot(sol, idxs=dpsi, lab=false, title="(A) Mito. memb. potential", xlabel="", ylabel="Voltage (mV)")
+pl_mmp = plot(sol, idxs=dpsi, lab=false, title="(A) Mito. memb. potential", xlabel="", ylabel="Voltage (mV)") |> PNG
 
 # Action potential
-pl_vm = plot(sol, idxs=vm, lab=false, title="(B) Action potential", xlabel="", ylabel="Voltage (mV)")
+pl_vm = plot(sol, idxs=vm, lab=false, title="(B) Action potential", xlabel="", ylabel="Voltage (mV)") |> PNG
 
 # ATP
-pl_atp = plot(sol, idxs=atp_i/1000, lab=false, title="(C) ATP", xlabel="", ylabel="Conc. (mM)")
+pl_atp = plot(sol, idxs=atp_i/1000, lab=false, title="(C) ATP", xlabel="", ylabel="Conc. (mM)") |> PNG
 
 # CAC
 @unpack cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m = sys
-pl_cac = plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m], legend=:right, title="(D) CAC intermediates", xlabel="Time (ms)", ylabel="Conc. (μM)")
+pl_cac = plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m], legend=:right, title="(D) CAC intermediates", xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
 
 # Q cycle
 @unpack Q_n, SQn, QH2_n, QH2_p, Q_p, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
-pl_q = plot(sol, idxs=[Q_n, Q_p, SQn, QH2_n, QH2_p], title="(E) Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+pl_q = plot(sol, idxs=[Q_n, Q_p, SQn, QH2_n, QH2_p], title="(E) Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
 
 # Downstream of Q cycle
-plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle downstream", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle downstream", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
 
 # Mito oxygen concentration
-plot(sol, idxs=sys.O2)
+plot(sol, idxs=sys.O2) |> PNG
 
 # O2 shunt
-pl_ros = plot(sol, idxs=100 * sys.vROS / (sys.vO2 + sys.vROS), title="(F) ROS generation", lab=false, xlabel="Time (ms)", ylabel="Fraction of O2 consumption (%)", ylims=(0, 100))
+pl_ros = plot(sol, idxs=100 * sys.vROS / (sys.vO2 + sys.vROS), title="(F) ROS generation", lab=false, xlabel="Time (ms)", ylabel="Fraction of O2 consumption (%)", ylims=(0, 100)) |> PNG
 
 # Superoxide production
-plot(sol, idxs=[sys.vROSC1, sys.vROSIf, sys.vROSIq, sys.vROSC3], ylims=(0.0, 0.05))
+plot(sol, idxs=[sys.vROSC1, sys.vROSIf, sys.vROSIq, sys.vROSC3], ylims=(0.0, 0.05)) |> PNG
 
 # Superoxide concentrations
-plot(sol, idxs=[sys.sox_m, sys.sox_i])
+plot(sol, idxs=[sys.sox_m, sys.sox_i]) |> PNG
 
 # Succinate dehydrogenase
-plot(sol, idxs=[sys.vSDH])
+plot(sol, idxs=[sys.vSDH]) |> PNG
 
 # Semiquinone from Complex III Qo
-plot(sol, idxs=[sys.SQp])
+plot(sol, idxs=[sys.SQp]) |> PNG
