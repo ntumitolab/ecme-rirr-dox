42ec765d7e9ceaca18332c72df986d8ae304a99a
sosiristseng <sosiristseng@gmail.com>
2025-07-08 06:58:40 +0000

    param dep

--- test.jl
+++ test.jl
@@ -1,19 +1,2 @@
 using OrdinaryDiffEq
 using ModelingToolkit
-using ModelingToolkit: t_nounits as t, D_nounits as D
-using DiffEqCallbacks
-using Plots
-
-@parameters begin
-    kf = 1
-    keq = 1
-    kb = kf / keq
-end
-
-@variables a(t) = 1 b(t) = 0
-
-v = kf * a - kb * b
-
-@mtkcompile sys = System([D(a) ~ -v; D(b) ~ v], t)
-
-prob = ODEProblem(sys, [], (0.0, 1.0))
