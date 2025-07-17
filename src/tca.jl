6bce4ac894f7b796b6eba51e2cc242301b199e5c
Wen Wei Tseng <sosiristseng@gmail.com>
2025-07-17 17:19:46 +0800

    wip

--- src/tca.jl
+++ src/tca.jl
@@ -1,33 +1,3 @@
-"Citrate synthase rate"
-function v_cs(oaa; accoa=10μM, KM_ACCOA_CS=12.6μM, KM_OAA_CS=0.64μM, KCAT_CS=50Hz, ET_CS=400μM)
-    return KCAT_CS * ET_CS * hil(oaa, KM_OAA_CS) * hil(accoa, KM_ACCOA_CS)
-end
-
-"Aconitase rate"
-function v_aco(cit, isoc; rKeq=inv(2.22), KF_ACO=12.5Hz)
-    return KF_ACO * (cit - isoc * rKeq)
-end
-
-"IDH3 (Isocitrate dehydrogenase, NADH-producing) rate (Wei, 2011)"
-function v_idh3(isoc, nad_m, nadh_m; ca_m, adp_m, h_m, KI_NADH_IDH=190μM, KCAT_IDH=60Hz, ET_IDH=109μM, KM_ISOC_IDH=1520μM, KM_NAD_IDH=923μM, KM_ADP_IDH=620μM, KM_CA_IDH=0.5μM, KH1_IDH=10nM, KH2_IDH=900nM)
-    vmax = KCAT_IDH * ET_IDH
-    a = (isoc / KM_ISOC_IDH)^2 * (1 + adp_m / KM_ADP_IDH) * (1 + ca_m / KM_CA_IDH)
-    b = nad_m / KM_NAD_IDH * hil(KI_NADH_IDH, nadh_m)
-    h = 1 + h_m / KH1_IDH + KH2_IDH / h_m
-    return vmax * a * b / (h * a * b + a + b + 1)
-end
-
-"Oxoglutarate dehydrogenase complex (OGDC / KGDH) rate (Wei, 2011)"
-function v_kgdh(akg, nad_m; ca_m, mg_m=400μM, h_m=exp10(-7.6) * Molar, ET_KGDH=500μM, KCAT_KGDH=50Hz, KM_AKG_KGDH=1940μM, KM_NAD_KGDH=38.7mM, KH1_KGDH=0.04μM, KH2_KGDH=0.07μM, KM_MG_KGDH=30.8μM, KM_CA_KGDH=0.15μM, NI_AKG_KGDH=1.2)
-    vmax = ET_KGDH * KCAT_KGDH
-    f_h = 1 + h_m / KH1_KGDH + KH2_KGDH / h_m
-    f_akg = NaNMath.pow(akg / KM_AKG_KGDH, NI_AKG_KGDH)
-    f_mgca = (1 + mg_m / KM_MG_KGDH) * (1 + ca_m / KM_CA_KGDH)
-    f_nad = nad_m / KM_NAD_KGDH
-    vmax = ET_KGDH * KCAT_KGDH
-    return vmax * f_akg * f_nad * f_mgca / (f_h * f_mgca * f_akg * f_nad + f_akg + f_nad)
-end
-
 "Succinyl-CoA lyase rate"
 function v_sl(scoa, adp_m, suc, atp_m; pi_m=8mM, COA=20μM, KF_SL=28 / (μM * ms), rKEQ_SL=inv(3.115))
     return KF_SL * (scoa * adp_m * pi_m - suc * atp_m * COA * rKEQ_SL)
@@ -60,7 +30,7 @@ function v_mdh(mal, oaa, nad_m; h_m=exp10(-7.6) * Molar, KCAT_MDH=126Hz, ET_MDH=
 end
 
 # TCA cycle model, default parameters values from Gauthier et al. (2013)
-function get_tca_sys(; atp_m, adp_m, nad_m, nadh_m, h_m=exp10(-7.6) * Molar, ca_m, pi_m=8mM, mg_m=0.4mM, use_mg=false, name=:tcasys)
+function get_tca_sys(; atp_m, adp_m, nad_m, nadh_m, ca_m, h_m=exp10(-7.6) * Molar, pi_m=8mM, mg_m=0.4mM, use_mg=false, name=:tcasys)
     @parameters begin
         # Total TCA metabolite pool
         TCA_T = 1.3mM
@@ -147,12 +117,40 @@ function get_tca_sys(; atp_m, adp_m, nad_m, nadh_m, h_m=exp10(-7.6) * Molar, ca_
         mal(t) = 160μM      # malate
     end
 
+    ## Citrate synthase
+    v_cs = KCAT_CS * ET_CS * hil(oaa, KM_OAA_CS) * hil(ACCOA, KM_ACCOA_CS)
+    ## Aconitase
+    v_aco = KF_ACO * (cit - isoc * rKEQ_ACO)
+    ## IDH3 (Isocitrate dehydrogenase, NADH-producing)
+    v_idh3 = let
+        vmax = KCAT_IDH * ET_IDH
+        A = (isoc / KM_ISOC_IDH)^2 * (1 + adp_m / KM_ADP_IDH) * (1 + ca_m / KM_CA_IDH)
+        B = nad_m / KM_NAD_IDH * hil(KI_NADH_IDH, nadh_m)
+        H = 1 + h_m / KH1_IDH + KH2_IDH / h_m
+        v_idh3 = vmax * A * B / (H * A * B + A + B + 1)
+    end
+    ## Oxoglutarate dehydrogenase complex (OGDC / KGDH)
+    v_kgdh = let
+        vmax = ET_KGDH * KCAT_KGDH
+        f_h = 1 + h_m / KH1_KGDH + KH2_KGDH / h_m
+        f_akg = NaNMath.pow(akg / KM_AKG_KGDH, NI_AKG_KGDH)
+        f_mgca = (1 + mg_m / KM_MG_KGDH) * (1 + ca_m / KM_CA_KGDH)
+        f_nad = nad_m / KM_NAD_KGDH
+        vmax = ET_KGDH * KCAT_KGDH
+        v_kgdh = vmax * f_akg * f_nad * f_mgca / (f_h * f_mgca * f_akg * f_nad + f_akg + f_nad)
+    end
+    ## Succinyl-CoA lyase
+    v_sl = let
+
+    end
+
+
     eqs = [
         TCA_T ~ cit + isoc + oaa + akg + scoa + suc + fum + mal,
-        vCS ~ v_cs(oaa; accoa=ACCOA, KCAT_CS, ET_CS, KM_ACCOA_CS, KM_OAA_CS),
-        vACO ~ v_aco(cit, isoc; rKeq=rKEQ_ACO, KF_ACO),
-        vIDH ~ v_idh3(isoc, nad_m, nadh_m; ca_m, adp_m, h_m, KI_NADH_IDH, KCAT_IDH, ET_IDH, KH1_IDH, KH2_IDH, KM_ISOC_IDH, KM_NAD_IDH, KM_ADP_IDH, KM_CA_IDH),
-        vKGDH ~ v_kgdh(akg, nad_m; ca_m, mg_m, h_m, ET_KGDH, KCAT_KGDH, KM_AKG_KGDH, KM_NAD_KGDH, KH1_KGDH, KH2_KGDH, KM_MG_KGDH, KM_CA_KGDH, NI_AKG_KGDH),
+        vCS ~ v_cs,
+        vACO ~ v_aco,
+        vIDH ~ v_idh3,
+        vKGDH ~ v_kgdh,
         vSL ~ use_mg ? v_sl2(scoa, adp_m, suc, atp_m; pi_m, COA, KF_SL, rKEQ_SL, h_m, mg_m) : v_sl(scoa, adp_m, suc, atp_m; pi_m, COA, KF_SL, rKEQ_SL),
         vFH ~ v_fh(fum, mal; KF_FH, rKEQ_FH),
         vMDH ~ v_mdh(mal, oaa, nad_m; h_m, KCAT_MDH, ET_MDH, KH1_MDH, KH2_MDH, KH3_MDH, KH4_MDH, K_OFFSET_MDH, KM_MAL_MDH, KI_OAA_MDH, KM_NAD_MDH),
