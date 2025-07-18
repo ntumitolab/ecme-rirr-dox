0df6131d356452d06ad9731cb69bc17dca56eb7f
Wen Wei Tseng <sosiristseng@gmail.com>
2025-07-18 17:30:22 +0800

    reversible AST (#55)

--- docs/hypoxia-reperfusion.jl
+++ docs/hypoxia-reperfusion.jl
@@ -16,7 +16,7 @@ bcl = 1.0second
 u0 = build_u0(sys)
 sts = unknowns(sys)
 alg = KenCarp47()
-prob = ODEProblem(sys, [u0; sys.O2_o => 6nM; sys.Em_N2 => -150mV], tend)
+prob = ODEProblem(sys, [u0; sys.O2_o => 6nM], tend)
 
 reoxygen! = (integrator) -> begin
     integrator.ps[sys.O2_o] = 6μM
@@ -48,39 +48,68 @@ plot(pl_mmp, pl_vm, pl_atp, pl_cac, pl_q, pl_ros, size=(1200, 800)) |> PNG
 
 # MMP
 @unpack vm, dpsi, atp_i, adp_i = sys
-pl_mmp = plot(sol, idxs=dpsi, lab=false, title="(A) Mito. memb. potential", xlabel="", ylabel="Voltage (mV)") |> PNG
+pl_mmp = plot(sol, idxs=dpsi, lab=false, title="(A) Mito. memb. potential", xlabel="", ylabel="Voltage (mV)")
+vline!(pl_mmp, [50.0second], lines=(:dash, :black), lab=false)
+pl_mmp |> PNG
 
 # Action potential
-pl_vm = plot(sol, idxs=vm, lab=false, title="(B) Action potential", xlabel="", ylabel="Voltage (mV)") |> PNG
+pl_vm = plot(sol, idxs=vm, lab=false, title="(B) Action potential", xlabel="", ylabel="Voltage (mV)")
+vline!(pl_vm, [50.0second], lines=(:dash, :black), lab=false)
+pl_vm |> PNG
 
 # ATP
-pl_atp = plot(sol, idxs=atp_i/1000, lab=false, title="(C) ATP", xlabel="", ylabel="Conc. (mM)") |> PNG
+pl_atp = plot(sol, idxs=atp_i/1000, lab=false, title="(C) ATP", xlabel="", ylabel="Conc. (mM)")
+vline!(pl_atp, [50.0second], lines=(:dash, :black), lab=false)
+pl_atp |> PNG
 
 # CAC
 @unpack cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m = sys
-pl_cac = plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m], legend=:right, title="(D) CAC intermediates", xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
+pl_cac = plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal, nadh_m], legend=:right, title="(D) CAC intermediates", xlabel="Time (ms)", ylabel="Conc. (μM)")
+vline!(pl_cac, [50.0second], lines=(:dash, :black), lab=false)
+pl_cac |> PNG
 
 # Q cycle
 @unpack Q_n, SQn, QH2_n, QH2_p, Q_p, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
-pl_q = plot(sol, idxs=[Q_n, Q_p, SQn, QH2_n, QH2_p], title="(E) Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
+pl_q = plot(sol, idxs=[Q_n, Q_p, SQn, QH2_n, QH2_p], title="(E) Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+vline!(pl_q, [50.0second], lines=(:dash, :black), lab=false)
+pl_q |> PNG
 
 # Downstream of Q cycle
-plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle downstream", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)") |> PNG
+pl = plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle downstream", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
+
+# Proton pumping
+pl = plot(sol, idxs=[sys.vHresC1, sys.vHresC3, sys.vHresC4], title="Proton pumping", legend=:left, xlabel="Time (ms)", ylabel="Rate (μM/ms)")
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
 
 # Mito oxygen concentration
-plot(sol, idxs=sys.O2) |> PNG
+pl = plot(sol, idxs=sys.O2)
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
 
 # O2 shunt
-pl_ros = plot(sol, idxs=100 * sys.vROS / (sys.vO2 + sys.vROS), title="(F) ROS generation", lab=false, xlabel="Time (ms)", ylabel="Fraction of O2 consumption (%)", ylims=(0, 100)) |> PNG
+pl_ros = plot(sol, idxs=100 * sys.vROS / (sys.vO2 + sys.vROS), title="(F) ROS generation", lab=false, xlabel="Time (ms)", ylabel="Fraction of O2 consumption (%)", ylims=(0, 100))
+vline!(pl_ros, [50.0second], lines=(:dash, :black), lab=false)
+pl_ros |> PNG
 
 # Superoxide production
-plot(sol, idxs=[sys.vROSC1, sys.vROSIf, sys.vROSIq, sys.vROSC3], ylims=(0.0, 0.05)) |> PNG
+pl = plot(sol, idxs=[sys.vROSC1, sys.vROSIf, sys.vROSIq, sys.vROSC3], ylims=(0.0, 0.05))
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
 
 # Superoxide concentrations
-plot(sol, idxs=[sys.sox_m, sys.sox_i]) |> PNG
+pl = plot(sol, idxs=[sys.sox_m, sys.sox_i])
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
 
-# Succinate dehydrogenase
-plot(sol, idxs=[sys.vSDH]) |> PNG
+# CAC flux
+pl = plot(sol, idxs=[sys.vSDH, sys.vAAT, sys.vMDH])
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
 
 # Semiquinone from Complex III Qo
-plot(sol, idxs=[sys.SQp]) |> PNG
+pl = plot(sol, idxs=[sys.SQp])
+vline!(pl, [50.0second], lines=(:dash, :black), lab=false)
+pl |> PNG
