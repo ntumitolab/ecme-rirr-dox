b578f9d0df73e64d14a5a38932da62625279746d
Wen Wei Tseng <sosiristseng@gmail.com>
2025-12-13 17:19:37 +0800

    CI

--- docs/index.jl
+++
@@ -1,9 +0,0 @@
-# # ECME-RIRR-DOX model
-# Cardiomyocyte electrophysiology and bioenergetics model with doxorubicin perturbation.
-# ## Runtime information
-using InteractiveUtils
-InteractiveUtils.versioninfo()
-
-#---
-using Pkg
-Pkg.status()
