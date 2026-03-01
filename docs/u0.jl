8e81cbd963e9d1d6c70cf429ebc79ce8442498ec
Wen Wei Tseng <sosiristseng@gmail.com>
2026-03-01 21:36:13 +0800

    wip

--- docs/u0.jl
+++ docs/u0.jl
@@ -24,3 +24,46 @@ prob = ODEProblem(sys, u0, tend)
 
 #---
 @time sol = solve(prob, alg; callback = callback, saveat=0.01second);
+
+for i in sts
+    istr = replace(string(i), "(t)" => "")
+    println("sys.", istr, " => ", sol[i][end], ",")
+end
+
+# ## Action potential
+plot(sol, idxs=sys.vm, legend=:right, tspan=(900second, 901second))
+
+# ## Citric acid cycle metabolites
+# Citrate and isocitrate have the highest concentrations.
+@unpack cit, isoc, oaa, akg, scoa, suc, fum, mal = sys
+plot(sol, idxs=[cit, isoc, oaa, akg, scoa, suc, fum, mal], legend=:right, title="CAC metabolites")
+
+# ## CAC flux
+@unpack vMDH, vAAT, vIDH = sys
+plot(sol, idxs=[vMDH, vAAT, vIDH], legend=:right, title="CAC flux")
+
+# ## Q cycle
+@unpack Q_n, SQn, QH2_n, QH2_p, Q_p, SQp, fes_ox, fes_rd, cytc_ox, cytc_rd = sys
+plot(sol, idxs=[Q_n + Q_p, SQn, QH2_n + QH2_p, SQp], title="Q cycle", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+
+#---
+plot(sol, idxs=[fes_ox, fes_rd, cytc_ox, cytc_rd], title="Q cycle (downstream)", legend=:left, xlabel="Time (ms)", ylabel="Conc. (μM)")
+
+# ## Proton pumping
+plot(sol, idxs = [sys.vHresC1, sys.vHresC3, sys.vHresC4], ylims=(0, 3))
+
+# ## ROS
+plot(sol, idxs = [sys.sox_i, sys.sox_m], tspan=(900e3, 910e3))
+
+#---
+plot(sol, idxs = [sys.vROSIf, sys.vROSIq, sys.vROSC1, sys.vROSC3], tspan=(900e3, 910e3))
+
+#---
+plot(sol, idxs=100 * sys.vROS / (sys.vO2 + sys.vROS), title="O2 Shunt", tspan=(900e3, 910e3), ylims=(0, 5))
+
+# ## MMP
+
+plot(sol, idxs = [sys.dpsi], tspan=(900e3, 910e3))
+
+#---
+plot(sol, idxs = [sys.vC5], tspan=(900e3, 910e3))
