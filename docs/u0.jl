aedea23cc5ab998eb8ad5ccd8c5aee0043a55660
Wen Wei Tseng <sosiristseng@gmail.com>
2026-03-01 17:10:15 +0800

    wip

---
+++ docs/u0.jl
@@ -0,0 +1,21 @@
+# # Initial conditions
+# 1Hz pacing for 1000 seconds.
+using OrdinaryDiffEq
+using ModelingToolkit
+using ModelingToolkit: t_nounits as t, D_nounits as D
+using ECMEDox
+using ECMEDox: second, mM, Hz, μM, mV
+using Plots
+#---
+tend = 1000.0second
+bcl = 1.0second
+@time @named sys = build_model()
+u0 = build_u0(sys)
+sts = unknowns(sys)
+alg = KenCarp47()
+@unpack iStim = sys
+callback = build_stim_callbacks(iStim, tend; period=bcl)
+prob = ODEProblem(sys, u0, tend)
+
+#---
+@time sol = solve(prob, alg; callback = callback, saveat=0.01second);
